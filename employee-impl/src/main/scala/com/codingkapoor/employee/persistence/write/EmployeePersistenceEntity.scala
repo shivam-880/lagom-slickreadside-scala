e057017d4f43bdc3a03236996ac89e9b9756346d
codingkapoor <mailtoshivamk@gmail.com>
2019-09-02 11:48:36 +0530

    emp persistent entity made fsm

--- employee-impl/src/main/scala/com/codingkapoor/employee/persistence/write/EmployeePersistenceEntity.scala
+++ employee-impl/src/main/scala/com/codingkapoor/employee/persistence/write/EmployeePersistenceEntity.scala
@@ -10,20 +10,36 @@ class EmployeePersistenceEntity extends PersistentEntity {
 
   override def initialState: Option[EmployeeState] = None
 
-  override def behavior: Behavior =
+  override def behavior: Behavior = {
+    case state if state.isEmpty => initial
+    case state if state.isDefined => postAdded
+  }
+
+  private val initial: Actions =
     Actions().onCommand[AddEmployee, Done] {
       case (AddEmployee(e), ctx, _) =>
         ctx.thenPersist(EmployeeAdded(e.id, e.name, e.gender, e.doj, e.pfn)) { _ =>
           ctx.reply(Done)
         }
+    }.onCommand[UpdateEmployee, Done]{
+      case(UpdateEmployee(e), ctx, _) =>
+        ctx.invalidCommand(s"No employee found with id = ${e.id}")
+        ctx.done
+    }.onEvent {
+      case (EmployeeAdded(id, name, gender, doj, pfn), _) =>
+        Some(EmployeeState(id, name, gender, doj, pfn))
+    }
+
+  private val postAdded: Actions =
+    Actions().onCommand[AddEmployee, Done]{
+      case (AddEmployee(e), ctx, _) =>
+        ctx.invalidCommand(s"Employee with id = ${e.id} already exists.")
+        ctx.done
     }.onCommand[UpdateEmployee, Done] {
       case (UpdateEmployee(e), ctx, _) =>
         ctx.thenPersist(EmployeeUpdated(e.id, e.name, e.gender, e.doj, e.pfn)) { _ =>
           ctx.reply(Done)
         }
-    }.onEvent {
-      case (EmployeeAdded(id, name, gender, doj, pfn), _) =>
-        Some(EmployeeState(id, name, gender, doj, pfn))
     }.onEvent {
       case (EmployeeUpdated(id, name, gender, doj, pfn), _) =>
         Some(EmployeeState(id, name, gender, doj, pfn))
