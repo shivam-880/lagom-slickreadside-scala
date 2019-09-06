265b7d0627c8b8ba52175c098d6e28d20587b7f0
codingkapoor <mailtoshivamk@gmail.com>
2019-09-06 19:04:44 +0530

    imports fixed

--- employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
+++ employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
@@ -5,17 +5,15 @@ import com.codingkapoor.employee.persistence.read.{EmployeeEventProcessor, Emplo
 import com.codingkapoor.employee.persistence.write.{EmployeePersistenceEntity, EmployeeSerializerRegistry}
 import com.codingkapoor.employee.service.EmployeeServiceImpl
 import com.lightbend.lagom.scaladsl.persistence.cassandra.WriteSideCassandraPersistenceComponents
-import com.lightbend.lagom.scaladsl.persistence.jdbc.ReadSideJdbcPersistenceComponents
 import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext}
-import com.lightbend.lagom.scaladsl.persistence.slick.SlickPersistenceComponents
+import com.lightbend.lagom.scaladsl.persistence.slick.ReadSideSlickPersistenceComponents
 import com.softwaremill.macwire._
 import play.api.db.HikariCPComponents
 import play.api.libs.ws.ahc.AhcWSComponents
 
 abstract class EmployeeApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
-    with ReadSideJdbcPersistenceComponents
-    with SlickPersistenceComponents
+    with ReadSideSlickPersistenceComponents
     with WriteSideCassandraPersistenceComponents
     with HikariCPComponents
     with AhcWSComponents {
